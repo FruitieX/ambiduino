0d7b6e11f894ef07b30fcb9ad6592c52dbbc189e
Rasmus Eskola <fruitiex@gmail.com>
2016-01-25 00:33:41 +0200

    initial commit

---
+++ ambiduino.ino
@@ -0,0 +1,66 @@
+#include "FastLED.h"
+
+// How many leds in your strip?
+#define NUM_LEDS 6
+
+// For led chips like Neopixels, which have a data line, ground, and power, you just
+// need to define DATA_PIN.  For led chipsets that are SPI based (four wires - data, clock,
+// ground, and power), like the LPD8806 define both DATA_PIN and CLOCK_PIN
+#define DATA_PIN 3
+//#define CLOCK_PIN 13
+
+// Define the array of leds
+CRGB leds[NUM_LEDS];
+
+byte CUR_LED = 0;
+
+void setup() { 
+      // Uncomment/edit one of the following lines for your leds arrangement.
+      // FastLED.addLeds<TM1803, DATA_PIN, RGB>(leds, NUM_LEDS);
+      // FastLED.addLeds<TM1804, DATA_PIN, RGB>(leds, NUM_LEDS);
+      // FastLED.addLeds<TM1809, DATA_PIN, RGB>(leds, NUM_LEDS);
+      // FastLED.addLeds<WS2811, DATA_PIN, RGB>(leds, NUM_LEDS);
+      // FastLED.addLeds<WS2812, DATA_PIN, RGB>(leds, NUM_LEDS);
+      FastLED.addLeds<WS2812B, DATA_PIN, GRB>(leds, NUM_LEDS);
+      // FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
+      // FastLED.addLeds<UCS1903, DATA_PIN, RGB>(leds, NUM_LEDS);
+      // FastLED.addLeds<UCS1903B, DATA_PIN, RGB>(leds, NUM_LEDS);
+      // FastLED.addLeds<GW6205, DATA_PIN, RGB>(leds, NUM_LEDS);
+      // FastLED.addLeds<GW6205_400, DATA_PIN, RGB>(leds, NUM_LEDS);
+      
+      // FastLED.addLeds<WS2801, RGB>(leds, NUM_LEDS);
+      // FastLED.addLeds<SM16716, RGB>(leds, NUM_LEDS);
+      // FastLED.addLeds<LPD8806, RGB>(leds, NUM_LEDS);
+
+      // FastLED.addLeds<WS2801, DATA_PIN, CLOCK_PIN, RGB>(leds, NUM_LEDS);
+      // FastLED.addLeds<SM16716, DATA_PIN, CLOCK_PIN, RGB>(leds, NUM_LEDS);
+      // FastLED.addLeds<LPD8806, DATA_PIN, CLOCK_PIN, RGB>(leds, NUM_LEDS);
+
+      Serial.begin(115200);
+
+      // request status for first led
+      Serial.write(CUR_LED);
+}
+
+void loop() { 
+  FastLED.show();
+}
+
+void serialEvent() {
+  // do we have all color components for the next LED?
+  if (Serial.available() < 3) {
+    return;
+  }
+
+  Serial.readBytes((char*)(&leds[NUM_LEDS - 1 - CUR_LED]), 3);
+  /*
+  CHSV hsv = rgb2hsv_approximate(leds[NUM_LEDS - 1 - CUR_LED]);
+  hsv.saturation = min(hsv.saturation * 2, 255);
+  hsv2rgb_rainbow(hsv, leds[NUM_LEDS - 1 - CUR_LED]);
+  */
+  
+  CUR_LED = (CUR_LED + 1) % NUM_LEDS;
+
+  Serial.write(CUR_LED);
+}
+
